c6b47ef25c2d5b227a73b6af4eb226d5fdcf122c
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2015-03-24 23:14:31 -0700

    Update user-setup.sh

--- user-setup.sh
+++ user-setup.sh
@@ -3,9 +3,9 @@
 cd /home/vagrant
 
 # yaourt packages
-# yaourt -S --noconfirm opam
-# yaourt -S --noconfirm emacs-tuareg-mode
-# yaourt -S --noconfirm auto-complete
+yaourt -S --noconfirm opam
+yaourt -S --noconfirm emacs-tuareg-mode
+yaourt -S --noconfirm auto-complete
 
 # opam packages
 OPAM_PACKAGES="async cohttp core_bench core_extended cryptokit js_of_ocaml menhir merlin oasis ocp-indent ocp-index ssl utop"
