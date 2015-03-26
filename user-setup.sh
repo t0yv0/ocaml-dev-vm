76861bf44c472b6bcef5cb59729d2626339642be
Anton Tayanovskyy (t0yv0) <anton.tayanovskyy@gmail.com>
2015-03-25 23:11:33 -0700

    another fix

--- user-setup.sh
+++ user-setup.sh
@@ -6,7 +6,7 @@ YAOURT_PKG="auto-complete emacs-tuareg-mode opam"
 yaourt -S --noconfirm $YAOURT_PKG
 
 OPAM_PKG="async cohttp core_bench core_extended cryptokit js_of_ocaml"
-OPAM_PKG="$(OPAM_PKG) menhir merlin oasis ocp-indent ocp-index ssl utop"
+OPAM_PKG="$OPAM_PKG menhir merlin oasis ocp-indent ocp-index ssl utop"
 opam init -a --yes
 eval `opam config env`
 opam switch 4.02.1 --yes
