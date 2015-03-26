28d0f765425f1fb3e3f75b3ba3d9a1b82e6b42d5
Anton Tayanovskyy (t0yv0) <anton.tayanovskyy@gmail.com>
2015-03-25 22:00:08 -0700

    Misc.

--- provision.sh
+++ provision.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # pacman packages
+PKG="chromium emacs git ttf-inconsolata unzip xfce4"
 sudo pacman -Syu --noconfirm
-sudo pacman -S emacs git unzip xfce4 --noconfirm
+sudo pacman -S $PKG --noconfirm
 
 # vagrant user setup
 sudo -u vagrant bash /vagrant/user-setup.sh
