3404e12d9421377fe49c31fd86791e0ace2bbfc1
Anton Tayanovskyy (t0yv0) <anton.tayanovskyy@gmail.com>
2015-03-25 21:10:28 -0700

    Emacs fixes

--- user-setup.sh
+++ user-setup.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
 
-cd /home/vagrant
-
-# yaourt packages
-# yaourt -S --noconfirm opam
-# yaourt -S --noconfirm emacs-tuareg-mode
-# yaourt -S --noconfirm auto-complete
-
-# opam packages
-OPAM_PACKAGES="async cohttp core_bench core_extended cryptokit js_of_ocaml menhir merlin oasis ocp-indent ocp-index ssl utop"
+cd ~
+YAOURT_PKG="auto-complete emacs-tuareg-mode opam"
+yaourt -S --noconfirm $YAOURT_PKG
+OPAM_PKG="async cohttp core_bench core_extended cryptokit js_of_ocaml"
+OPAM_PKG="$(OPAM_PKG) menhir merlin oasis ocp-indent ocp-index ssl utop"
 opam init -a --yes
 eval `opam config env`
 opam switch 4.02.1 --yes
 opam install $OPAM_PACKAGES --yes
 
 # config files
-git clone https://github.com/samoht/ocaml-emacs-settings.git
-cp ocaml-emacs-settings/.emacs ~/.emacs
-cp ocaml-emacs-settings/.emacs.d/* ~/.emacs.d/
-cat > ~/.ocamlinit <<EOF
-#use "topfind";;
-#thread;;
-#camlp4o;;
-EOF
+cp /vagrant/.emacs ~/.emacs
+cp /vagrant/.ocamlinit ~/.ocamlinit
